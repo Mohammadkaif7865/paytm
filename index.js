8cae71c0eeb1131410d343b4d61be74073edee91
mohammad kaif <mohammadkaif051197@gmail.com>
2022-09-13 19:49:49 +0530

    yes me

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post("/paynow", [parseUrl, parseJson], (req, res) => {
 });
 app.post("/callback", (req, res) => {
   // Route for verifiying payment
-
+  res.preventDefault();
   var body = '';
 
   req.on('data', function (data) {
